197e19b0e6db06a524947da5659543dba272533f
BraddLTU <93556575+BraddLTU@users.noreply.github.com>
2022-05-16 09:04:52 +1000

    listen icon and java test

---
+++ js/conifrm-test.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  let text;
+  if (confirm("Press a button!") == true) {
+    text = "You pressed OK!";
+  } else {
+    text = "You canceled!";
+  }
+  console.log(text);
+}
